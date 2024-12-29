01666d5d3d03d25578950a9f26084d3df86dd501
gdoc77 <gdakovich3@gmail.com>
2024-12-28 18:00:52 -0800

    Update ref

--- bin/tic_tac_toe_backend_cdk.ts
+++ bin/tic_tac_toe_backend_cdk.ts
@@ -4,7 +4,7 @@ import { TicTacToeBackendCdkStack } from '../lib/tic_tac_toe_backend_cdk-stack';
 import { PipelineStack } from '../lib/pipeline/pipeline';
 
 const app = new App();
-const ticTacToeStack = new TicTacToeBackendCdkStack(app, 'TicTacToeBackendCdkStack', {
+const ticTacToeStack = new TicTacToeBackendCdkStack(app, 'TicTacToeBackendCdk', {
   env: { account: "955625928616", region: "us-west-2" },//account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
