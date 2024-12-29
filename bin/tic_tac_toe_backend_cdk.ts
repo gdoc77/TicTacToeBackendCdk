97a7864672175df04d442b4545a1ecf489936f65
gdoc77 <gdakovich3@gmail.com>
2024-12-28 18:20:48 -0800

    Revert "Update stack name"
    This reverts commit ca9df843290e6d8fb402a87f7dfab3e8842023b9.

--- bin/tic_tac_toe_backend_cdk.ts
+++ bin/tic_tac_toe_backend_cdk.ts
@@ -3,7 +3,7 @@ import { App } from 'aws-cdk-lib';
 import { TicTacToeBackendCdkStack } from '../lib/tic_tac_toe_backend_cdk-stack';
 
 const app = new App();
-const ticTacToeStack = new TicTacToeBackendCdkStack(app, 'TicTacToeBackendCdkStack', {
+const ticTacToeStack = new TicTacToeBackendCdkStack(app, 'TicTacToeBackendCdk', {
   env: { account: "955625928616", region: "us-west-2" },//account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
