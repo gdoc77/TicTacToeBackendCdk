3814b5112fcd3fac8c9a2c53803bd0eb1559fe55
gdoc77 <gdakovich3@gmail.com>
2024-12-28 17:24:41 -0800

    Add codepipeline

--- bin/tic_tac_toe_backend_cdk.ts
+++ bin/tic_tac_toe_backend_cdk.ts
@@ -1,9 +1,14 @@
 #!/usr/bin/env node
-import * as cdk from 'aws-cdk-lib';
+import { App } from 'aws-cdk-lib';
 import { TicTacToeBackendCdkStack } from '../lib/tic_tac_toe_backend_cdk-stack';
+import { PipelineStack } from '../lib/pipeline/pipeline';
 
-const app = new cdk.App();
-new TicTacToeBackendCdkStack(app, 'TicTacToeBackendCdkStack', {
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const app = new App();
+const ticTacToeStack = new TicTacToeBackendCdkStack(app, 'TicTacToeBackendCdkStack', {
+  env: { account: "955625928616", region: "us-west-2" },//account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
 });
+
+const pipelineStack = new PipelineStack(app, 'TicTacToePipeline');
+
+pipelineStack.addDependency(ticTacToeStack);
---
+++ lib/pipeline/pipeline.ts
@@ -0,0 +1,101 @@
+import {
+    Stack,
+    StackProps,
+    SecretValue
+} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as codepipeline from 'aws-cdk-lib/aws-codepipeline';
+import * as codepipeline_actions from 'aws-cdk-lib/aws-codepipeline-actions';
+import * as codebuild from 'aws-cdk-lib/aws-codebuild';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class PipelineStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    // S3 Bucket for Pipeline Artifacts
+    const artifactBucket = new s3.Bucket(this, 'TicTacToePipelineArtifactsBucket', {
+      versioned: true,
+    });
+
+    // Source and Build Artifacts
+    const sourceArtifact = new codepipeline.Artifact();
+    const buildArtifact = new codepipeline.Artifact();
+
+    // CodeBuild Project for Synth
+    const cdkBuildProject = new codebuild.PipelineProject(this, 'TicTacToeCdkBuildProject', {
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+      },
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: [
+              'npm install',
+            ],
+          },
+          build: {
+            commands: [
+              'npm run build',
+              'npx cdk synth --output dist',
+            ],
+          },
+        },
+        artifacts: {
+          'base-directory': 'dist',
+          files: '**/*',
+        },
+      }),
+    });
+
+    // Define CodePipeline
+    const pipeline = new codepipeline.Pipeline(this, 'TicTacToePipeline', {
+      pipelineName: 'TicTacToeDeployPipeline',
+      artifactBucket: artifactBucket,
+    });
+
+    // Source Stage
+    pipeline.addStage({
+      stageName: 'Source',
+      actions: [
+        new codepipeline_actions.GitHubSourceAction({
+          actionName: 'GitHub_Source',
+          output: sourceArtifact,
+          oauthToken: SecretValue.secretsManager(
+            '/github/tictactoecdk-github-token', { jsonField: "github-token" } // Store GitHub token in Secrets Manager
+          ),
+          owner: 'gdoc77',
+          repo: 'TicTacToeBackendCdk',
+          branch: 'main',
+        }),
+      ],
+    });
+
+    // Build Stage
+    pipeline.addStage({
+      stageName: 'Build',
+      actions: [
+        new codepipeline_actions.CodeBuildAction({
+          actionName: 'Build_CDK',
+          project: cdkBuildProject,
+          input: sourceArtifact,
+          outputs: [buildArtifact],
+        }),
+      ],
+    });
+
+    // Deploy Stage
+    pipeline.addStage({
+      stageName: 'Deploy',
+      actions: [
+        new codepipeline_actions.CloudFormationCreateUpdateStackAction({
+          actionName: 'Deploy_CDK',
+          stackName: 'TicTacToeBackendCdkStack',
+          templatePath: buildArtifact.atPath('TicTacToeBackendCdk.template.json'),
+          adminPermissions: true,
+        }),
+      ],
+    });
+  }
+}
