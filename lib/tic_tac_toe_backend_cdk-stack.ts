7de97a6b9fc88ae3a212602a0eef68c99e56d2fe
gdoc77 <gdakovich3@gmail.com>
2024-12-29 12:11:13 -0800

    Scope instead of this

--- lib/tic_tac_toe_backend_cdk-stack.ts
+++ lib/tic_tac_toe_backend_cdk-stack.ts
@@ -1,13 +1,13 @@
-import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { PipelineStack } from './pipeline/pipeline';
 import { S3TestBucketStack } from './s3/s3_test_stack';
 
-export class TicTacToeBackendCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+export class TicTacToeBackendCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     const pipelineStack = new PipelineStack(this, 'TicTacToePipeline');
-    const s3TestBucketStack = new S3TestBucketStack(this, 'S3TestBucketStack');
+    const s3TestBucketStack = new S3TestBucketStack(scope, 'S3TestBucketStack');
   }
 }
