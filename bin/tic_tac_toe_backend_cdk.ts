3f2b02d9b86b49b9e2664d06c5e2681a4b566140
gdoc77 <gdakovich3@gmail.com>
2024-12-28 18:09:10 -0800

    Add test s3 bucket

--- bin/tic_tac_toe_backend_cdk.ts
+++ bin/tic_tac_toe_backend_cdk.ts
@@ -2,6 +2,7 @@
 import { App } from 'aws-cdk-lib';
 import { TicTacToeBackendCdkStack } from '../lib/tic_tac_toe_backend_cdk-stack';
 import { PipelineStack } from '../lib/pipeline/pipeline';
+import { S3TestBucketStack } from '../lib/s3/s3_test_stack';
 
 const app = new App();
 const ticTacToeStack = new TicTacToeBackendCdkStack(app, 'TicTacToeBackendCdk', {
@@ -10,5 +11,7 @@ const ticTacToeStack = new TicTacToeBackendCdkStack(app, 'TicTacToeBackendCdk', 
 });
 
 const pipelineStack = new PipelineStack(app, 'TicTacToePipeline');
+const s3TestBucketStack = new S3TestBucketStack(app, 'S3TestBucketStack')
 
 pipelineStack.addDependency(ticTacToeStack);
+s3TestBucketStack.addDependency(ticTacToeStack);
---
+++ lib/s3/s3_test_stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+
+export class S3TestBucketStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // Create an S3 Bucket
+    new s3.Bucket(this, 'MyTestBucket', {
+      bucketName: 'my-test-bucket-cdk', // Customize or remove for a unique name
+      versioned: true, // Enable versioning
+      publicReadAccess: false, // Disable public access
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // Automatically delete during stack removal
+    });
+  }
+}
